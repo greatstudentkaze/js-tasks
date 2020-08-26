62983c9e098d035b9673e2bd508248c9fea474b9
gsk <dfyz0r@list.ru>
2020-08-26 20:16:05 +0300

    реализует функцию, которая вычисляет стоимость товаров в корзине с учетом всевозможных скидок
    В качестве аргументов функция принимает три параметра:
    Стоимость всех товаров
    Количество товаров в корзине
    Промокод (его может не быть)
    
    Порядок начисления скидок важен и скидки могут суммироваться:
    
    1) При количестве товаров в корзине ≥ 5 применяется скидка 5% ко всей сумме, если ≥ 10 то 10%
    2) При сумме, превышающей 100 000, применяется скидка 20% к сумме превышения (Если сумма 150 000, то скидка только на 50 000).
    3) Если промокод '15', то ко всей сумме применяется скидка 15%, но только если сумма ≥ 25 000.
    4) Если промокод '100', то из суммы вычитается 100 рублей. При этом, если сумма меньше 100 рублей, итоговая стоимость будет 0.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const calcCost = (totalCost, goodsAmount, promoCode) => {
+  if (goodsAmount >= 10) {
+    // Скидка 10%
+    totalCost *= 0.9;
+  } else if (goodsAmount >= 5) {
+    // Скидка 5%
+    totalCost *= 0.95;
+  }
+
+  if (totalCost >= 100000) {
+    totalCost -= (totalCost - 100000) * 0.2;
+  }
+
+  if (promoCode === '15' && totalCost >= 25000) {
+    // Скидка 15%
+    totalCost *= 0.85;
+  } else if (promoCode === '100') {
+    if (totalCost >= 100) {
+      // Скидка 100
+      totalCost -= 100;
+    } else totalCost = 0;
+  }
+
+  return totalCost;
+}
+
+console.log(calcCost(200000, 5, '15')); // 146200
+console.log(calcCost(150000, 1, '15')); // 119000
+console.log(calcCost(150000, 5, '15')); // 113900
+console.log(calcCost(150000, 10, '100')); // 127900
+console.log(calcCost(102000, 2)); // 101600
+console.log(calcCost(105000, 11, '15')); // 80325
+console.log(calcCost(110, 10, '100')); // 0
+console.log(calcCost(27000, 11, '15')); // 24300
+console.log(calcCost(27000, 5, '15')); // 21802,5
+console.log(calcCost(22000, 2)); // 22000
+console.log(calcCost(22000, 2, '15')); // 22000
+console.log(calcCost(22000, 2, '100')); // 21900
