c7adceb26d9e5d9166046c62a3fc70ccb6ebf434
gsk <dfyz0r@list.ru>
2020-08-30 12:41:56 +0300

    реализует функцию isSomeTrue
    Написать фукнцию 'isSomeTrue', которая принимает 2 параметра - 'array' и 'func'
    'array' - массив
    'func' - фильтрующая функция
    
    Условия:
      1) Если фильтрующая функция вернет 'true' хотя бы для одного элемента массива, то и сама 'isSomeTrue' вернет 'true'.
      2) Если фильтрующая функция вернет 'false' для ВСЕХ элементов массива, то и сама 'isSomeTrue' вернет 'false'.
      3) Нельзя использовать функции для работы с массивами, методы и циклы.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const isSomeTrue = (array, func) => {
+  let result = func(array[0]);
+
+  if (array.length > 1 && !result) {
+    array.splice(0, 1);
+    let currentResult = isSomeTrue(array, func);
+    result = result || currentResult;
+  }
+
+  return result;
+};
+
+function isNumber(val) {
+  return typeof val === 'number';
+}
+
+let allNumbers= [1, 2, 4, 5, 6, 7, 8],
+  someNumbers = [1, 2, 'Hello', 4, 5, 'world', 6, 7, 8],
+  noNumbers = ['здесь', 'нет',  'чисел'],
+  testXd = ['xd', 1, 2, 3, 4];
+
+console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
+console.log(isSomeTrue(testXd, isNumber)); //вернет true
