c65bd15fb0392761e2ff4053431a853ea7083a29
gsk <dfyz0r@list.ru>
2020-12-31 11:18:13 +0300

    add a new task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+/*
+Задача из собеседования.
+Переверните каждое слово в строке. Например, строка
+"Welcome to this Javascript Guide!" должна стать "emocleW ot siht tpircsavaJ !ediuG".
+ */
+
+const task1 = () => {
+  const string = 'Welcome to this Javascript Guide!';
+  const trueString = 'emocleW ot siht tpircsavaJ !ediuG';
+
+  const reverse = (string, separator) => string.split(separator).reverse().join(separator);
+
+  const reversedString = reverse(string, '');
+  const result = reverse(reversedString, ' ');
+
+  console.log(trueString === result);
+};
